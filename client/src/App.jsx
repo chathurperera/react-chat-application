4eaad2bbd431952e448a59eaea83d08880d1ce75
chathurperera <chathuraperera007@gmail.com>
2022-07-13 02:43:56 +0530

    chat app UI done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import io from "socket.io-client";
+import Chat from "./Chat";
+import "./App.css";
 
+const socket = io.connect("http://localhost:3001");
 const App = () => {
+  const [userName, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  
+  const joinRoom = () => {
+    if ((userName !== "") & (room !== "")) {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join a chat room</h3>
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUserName(e.target.value)}
+            name=""
+            id=""
+          />
+          <input
+            type="text"
+            name=""
+            placeholder="Room"
+            onChange={(e) => setRoom(e.target.value)}
+            id=""
+          />
+          <button onClick={joinRoom}>Join Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} userName={userName} room={room} />
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
